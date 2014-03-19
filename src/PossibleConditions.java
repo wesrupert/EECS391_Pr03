98a3887ea0c2c4c146d665b80cf316ad40bd7158
Wes Rupert <ora@outlook.com>
2014-03-19 18:58:54 -0400

    Add possible variable combinations generator

--- src/PossibleConditions.java
+++ src/PossibleConditions.java
@@ -1,54 +1,82 @@
 import java.util.List;
+import java.util.Map;
+import java.util.HashMap;
 import java.util.ArrayList;
 
 class PossibleConditions {
-    private List<Condition> holding;
-    private List<Condition> at;
-    private List<Condition> contains;
+    private int[] ids, locations, types;
+    private List<Condition> holding, at, contains;
+    private List<Map<String, Value>> states;
 
-    public PossibleConditions(String[] ids, String[] locations, String[] types) {
-        holding = condList("Holding", ids, types);
-        at = condList("At", ids, locations);
-        contains = condList("Contains", locations, types);
+    public PossibleConditions(int[] ids, int[] locations, int[] types) {
+        this.ids = ids;
+        this.locations = locations;
+        this.types = types;
+        holding = at = contains = null;
+        states = null;
     }
 
-    /**
-     * @return the holding
-     */
     public List<Condition> getHolding() {
+        if (holding == null) {
+            holding = condList("Holding", ids, types);
+        }
         return holding;
     }
 
-    /**
-     * @return the at
-     */
     public List<Condition> getAt() {
+        if (at == null) {
+            at = condList("At", ids, locations);
+        }
         return at;
     }
 
-    /**
-     * @return the contains
-     */
     public List<Condition> getContains() {
+        if (contains == null) {
+            contains = condList("Contains", locations, types);
+        }
         return contains;
     }
 
     public List<Condition> getConditions() {
         List<Condition> conditions = new ArrayList<>();
-        conditions.addAll(holding);
-        conditions.addAll(at);
-        conditions.addAll(contains);
+        conditions.addAll(getHolding());
+        conditions.addAll(getAt());
+        conditions.addAll(getContains());
         return conditions;
     }
 
-    private List<Condition> condList(String name, String[] vars1, String[] vars2) {
+    public List<Map<String, Value>> getStates() {
+        if (states == null) {
+            states = statesList();
+        }
+        return states;
+    }
+
+
+    private List<Condition> condList(String name, int[] vars1, int[] vars2) {
         List<Condition> list = new ArrayList<>();
-        for (String var1 : vars1) {
-            for (String var2 : vars2) {
+        for (int var1 : vars1) {
+            for (int var2 : vars2) {
                 List<String> vars = new ArrayList<>();
-                vars.add(var1);
-                vars.add(var2);
+                vars.add(String.valueOf(var1));
+                vars.add(String.valueOf(var2));
                 list.add(new Condition("Holding", vars));
+            }
+        }
+        return list;
+    }
+
+    private List<Map<String, Value>> statesList() {
+        List<Map<String, Value>> list = new ArrayList<>();
+        for (int id : ids) {
+            for (int location : locations) {
+                for (int type : types) {
+                    Map<String, Value> map = new HashMap<>();
+                    map.put("id", new Value(id));
+                    map.put("pos", new Value(location));
+                    map.put("type", new Value(type));
+                    list.add(map);
+                }
             }
         }
         return list;
